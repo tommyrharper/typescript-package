11c5f40655e6a5af591df3119c5c439550997cee
Paulo Choi <mail@paulochoi.com>
2021-05-15 17:58:28 -0400

    Commit package.json

--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
 import { useState } from 'react';
+import {atom, useAtom } from 'jotai'
 const something = 5
+
+const test = atom('test')
+
 
 export function thing(arg: number) {
   const [data, setData] = useState(0)
+  const [data2, setData2] = useAtom(test)
+
   console.log(arg)
   return data
 }
